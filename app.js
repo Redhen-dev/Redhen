76d7a66873092ee61d6e89515bb80eefeafa1caf
Redhen-dev <red.hen.tai@outlook.com>
2018-11-11 15:28:39 +0000

    view

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
+
 app.get('/', function (req, res) {
-    res.send(JSON.stringify({ Hello: 'World'}));
+    res.send('index');
 });
 app.listen(port, function () {
     console.log('Example app listening on port !');
