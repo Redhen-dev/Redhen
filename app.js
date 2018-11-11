00f5e984b6281f2f6168a09485687d2b10fe754d
Redhen-dev <red.hen.tai@outlook.com>
2018-11-11 16:43:25 +0000

    Added view engine

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
+var path = require('path');
 var app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', function (req, res) {
-    res.send('index');
+    res.render('index');
 });
 app.listen(port, function () {
     console.log('Example app listening on port !');
